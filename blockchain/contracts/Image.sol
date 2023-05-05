7377267edf2a1e9e5da7e1bf541d95712fb937b4
Apurba Adhikari <apurba4790@gmail.com>
2023-05-05 22:40:13 +0545

    image finished

--- blockchain/contracts/Image.sol
+++ blockchain/contracts/Image.sol
@@ -2,36 +2,51 @@
 pragma solidity ^0.8.0;
 
 contract Image {
-    
     string public ipfsId;
-    address[] public acceptedAddresses;
-    address[] public rejectedAddresses;
+    address[] public addressAccepted;
+    address[] public addressRejected;
 
     function addToAccepted(address newAddress) public {
-        require(!isAccepted(newAddress), "Address already accepted");
-        acceptedAddresses.push(newAddress);
+        require(isAccepted(newAddress) != -1, "Address already accepted");
+
+        int rej = isRejected(newAddress);
+
+        if (rej != -1){
+            delete addressRejected[uint(rej)];
+        }
+
+        addressAccepted.push(newAddress);
+
     }
 
     function addToRejected(address newAddress) public {
-        require(!isRejected(newAddress), "Address already rejected");
-        rejectedAddresses.push(newAddress);
+        require(isRejected(newAddress) != -1, "Address already rejected");
+
+        int apt = isAccepted(newAddress);
+
+        if (apt != -1){
+            delete addressAccepted[uint(apt)];
+        }
+        addressRejected.push(newAddress);
     }
 
-    function isAccepted(address queryAddress) public view returns (bool) {
-        for (uint i = 0; i < acceptedAddresses.length; i++) {
-            if (acceptedAddresses[i] == queryAddress) {
-                return true;
+    function isAccepted(address queryAddress) public view returns (int) {
+        for (uint i = 0; i < addressAccepted.length; i++) {
+            if (addressAccepted[i] == queryAddress) {
+                return int(i);
             }
         }
-        return false;
+        return -1;
     }
 
-    function isRejected(address queryAddress) public view returns (bool) {
-        for (uint i = 0; i < rejectedAddresses.length; i++) {
-            if (rejectedAddresses[i] == queryAddress) {
-                return true;
+    function isRejected(address queryAddress) public view returns (int) {
+        for (uint i = 0; i < addressRejected.length; i++) {
+            if (addressRejected[i] == queryAddress) {
+                return int(i);
             }
         }
-        return false;
+        return -1;
     }
+
+
 }

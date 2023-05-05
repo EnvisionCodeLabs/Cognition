cecb9a7a1822a279f532a328ee0ddd301f14ef87
Samyam <samyamshr07@gmail.com>
2023-05-06 02:44:42 +0545

    userlanding page completion 80%

--- src/component/navbar.js
+++ src/component/navbar.js
@@ -8,7 +8,6 @@ class NoLoginHeader extends HTMLElement {
             </div>
             <ul class="flex">
                 <li><a href="./login.html">Login</a></li>
-                <li><a href="./register.html">Sign Up</a></li>
             </ul>
         </header>
     </nav>
@@ -24,12 +23,15 @@ class LoginHeader extends HTMLElement {
             <div>
                 <p>Cognition</p>
             </div>
-            <ul class="flex">
-                <li><a href="./login.html">Login</a></li>
-                <li><a href="./register.html">Sign Up</a></li>
-            </ul>
-            <div>
-                <p>Hello wrold</p>
+             <ul class="flex gap-20">
+            //     <li><a href="./login.html"><i class="fa-solid fa-house text-xl"></i></a></li>
+            //     <li><a href="./register.html"><i class="fa-solid fa-hashtag text-xl"></i></li>
+            //     <li><a href="./register.html"><i class="fa-solid fa-pen text-xl"></i></li>
+            //     <li><a href="./register.html"><i class="fa-solid fa-user text-xl"></i></li>
+            // </ul>
+            <div class="flex gap-5">
+                <p class="mt-1 font-bold">Hello wrold</p>
+                <img src="" class="profile-img-nav" />
             </div>
         </header>
     </nav>

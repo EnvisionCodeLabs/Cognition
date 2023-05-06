9a98b910737f58cdfca9e4d42ca789f15c1e3abf
Apurba Adhikari <apurba4790@gmail.com>
2023-05-06 23:10:01 +0545

    machine learning done

--- weviate/main.py
+++ weviate/main.py
@@ -1,14 +1,16 @@
 import weaviate
+from fastapi import FastAPI, File, UploadFile
+from typing import List
+import base64
+from fastapi.responses import JSONResponse
+import tempfile
+
 
 client = weaviate.Client("http://127.0.0.1:8080")
 
 # res = client.schema.get()
 
 # print(res)
-
-from fastapi import FastAPI, File, UploadFile
-from typing import List
-import base64
 
 app = FastAPI()
 
@@ -29,17 +31,23 @@ async def upload_image(file: UploadFile = File(...), hash: str = None):
     
     return "The image has been created"
     
-@app.post('/')
-async def compare_image(file: UploadFile = File(...)):
-    
+
+
+
+@app.post('/compare')
+async def compare_image(file: UploadFile = File(...)) -> JSONResponse:
     contents = await file.read()
-    encoded = base64.b64encode(contents).decode("utf-8")
-    
-    ret = await client.query.get("Images", ['image']).with_near_image({'image': encoded}).with_limit(3)
-    
+    encoded = base64.b64encode(contents).decode('utf-8')
+    decoded = base64.b64decode(encoded)
+    with tempfile.NamedTemporaryFile(delete=False) as f:
+        f.write(decoded)
+        image_path = f.name
+    ret = client.query.get("Images", ['image']).with_near_image({
+        'image': image_path
+    }).with_limit(3)
     res = ret.do()
-    
-    return 1
+    return JSONResponse(content=res)
+
 
 
 if __name__ == "main.py":

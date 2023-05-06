c55cecc9922dd221c89e7ef9ba51c564baa3012a
Ashwot Acharya <ashwot1133@gmail.com>
2023-05-06 13:03:08 +0545

    created a function to test cosine similarity

--- MachineLearning/main.py
+++ MachineLearning/main.py
@@ -8,25 +8,44 @@ from tensorflow.keras import datasets, layers, models
 
 
   
-image = cv2.imread('test.jpg')
-img = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
-  
+image1 = cv2.imread('test.jpg')
+image2 = cv2.imread('test3.jpg')
+img = cv2.cvtColor(image1, cv2.COLOR_BGR2GRAY)
+img2 = cv2.cvtColor(image2, cv2.COLOR_BGR2GRAY)
+
 
 width = 256
 height = 256
+
 dim = (width, height)
   
 resized = cv2.resize(img, dim, interpolation = cv2.INTER_AREA)
- 
+resized2 = cv2.resize(img2, dim, interpolation = cv2.INTER_AREA)
+
+
 image_array_normalized = np.array(resized / 256)
+image_array_normalized2= np.array(resized2 / 256)
+
 
 image_array_normalized = tf.reshape(image_array_normalized, [1, 256, 256, 1])
-max_pool_2d = tf.keras.layers.MaxPooling2D(pool_size=(2, 2), strides=(1, 1), padding='valid')
-new_ar = max_pool_2d( image_array_normalized )
-print(new_ar.shape)
-g = tf.reshape(new_ar, [255,255])
-cv2.imshow('wtf', g.numpy())
+image_array_normalized2 = tf.reshape(image_array_normalized2, [1, 256, 256, 1])
 
-print(g.numpy())
+
+max_pool_2d = tf.keras.layers.MaxPooling2D(pool_size=(1, 1), strides=(2, 2), padding='valid')
+max_pool_2d2 = tf.keras.layers.MaxPooling2D(pool_size=(1, 1), strides=(2, 2), padding='valid')
+
+new_ar = max_pool_2d( image_array_normalized )
+new_ar2 = max_pool_2d2( image_array_normalized2 )
+
+
+reshaped_gray_scale_image = tf.reshape(new_ar, [128,128])
+reshaped_gray_scale_image2 = tf.reshape(new_ar2, [128,128])
+
+cv2.imshow('test', reshaped_gray_scale_image.numpy())
+cv2.imshow('test2', reshaped_gray_scale_image2.numpy())
+
 cv2.waitKey(0)
 cv2.destroyAllWindows()
+
+def get_gray_image():
+    return [reshaped_gray_scale_image.numpy() , reshaped_gray_scale_image2.numpy()]

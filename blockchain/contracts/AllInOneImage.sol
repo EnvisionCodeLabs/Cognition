d85d3297327a00a01918f0573e93015d29a4fd85
Apurba Adhikari <apurba4790@gmail.com>
2023-05-07 02:13:11 +0545

    new contract created

---
+++ blockchain/contracts/AllInOneImage.sol
@@ -0,0 +1,50 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.0;
+
+contract AllInOneImage{
+
+    struct Image {
+        string ipfsId;
+        string fileName;
+        address owner;
+        bool confirmed;
+
+        uint yesCount;
+        uint noCount;
+    }
+
+    event ImageCreated (
+        string ipfsId,
+        string fileName
+    );
+
+    event VoteStarted(
+        string ipfsId,
+        string fileName
+    );
+
+    mapping(uint => Image) public images;
+
+    uint public currentCount;
+
+    constructor(){
+        currentCount = 0;
+    }
+
+    function addImage(string memory _ipfs, string memory fileName, bool confirmed) public payable {
+
+        images[currentCount] = Image(_ipfs, fileName, msg.sender, confirmed, 0, 0);
+
+        if (confirmed){
+            emit ImageCreated(_ipfs, fileName);
+        }else {
+            emit VoteStarted(_ipfs, fileName);
+        }
+
+        
+
+    }
+
+
+
+}
---
+++ react-frontend/src/contracts/allInOneImage.js
@@ -0,0 +1,4 @@
+import abi from "../abi/AllInOneImage.json"
+
+export const imageTokenAddress = "0x32df0145bBb8778e5ec563f8717E21170385bB38"
+export const allImageAbi = abi.abi
